db4914f2f4840d40c03299dc95c12faac4493066
Yang Zhang <yaaang@gmail.com>
2020-11-27 17:38:07 -0800

    Sync Plasmic demo component

---
+++ src/components/plasmic/3_minute_codegen_quickstart/icons/PlasmicIcon__Facebook.tsx
@@ -0,0 +1,39 @@
+// @ts-nocheck
+/* eslint-disable */
+/* tslint:disable */
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export type FacebookIconProps = React.ComponentProps<"svg"> & {
+  title?: string;
+};
+
+export function FacebookIcon(props: FacebookIconProps) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      aria-hidden={"true"}
+      fill={"currentColor"}
+      data-icon={"facebook"}
+      viewBox={"64 64 896 896"}
+      height={"1em"}
+      width={"1em"}
+      className={classNames("plasmic-default__svg", className)}
+      style={style}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path
+        d={
+          "M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zm-92.4 233.5h-63.9c-50.1 0-59.8 23.8-59.8 58.8v77.1h119.6l-15.6 120.7h-104V912H539.2V602.2H434.9V481.4h104.3v-89c0-103.3 63.1-159.6 155.3-159.6 44.2 0 82.1 3.3 93.2 4.8v107.9z"
+        }
+      ></path>
+    </svg>
+  );
+}
+
+export default FacebookIcon;
+/* prettier-ignore-end */
